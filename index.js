443419ca0d1922f0fece6b055b411ae43a0c5164
sun4205 <hanna4205@gmail.com>
2023-04-09 08:19:22 -0400

    practice multiplication

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ function checkAge(){
 
 let checking = checkAge();
 console.log(checking);
+
+for(var i=2; i<=9; i++){
+    for(var j=2; j<=9; j++){
+        console.log(i+"*"+j+"="+i*j);
+    }
+}
