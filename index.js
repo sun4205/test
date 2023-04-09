e0cbd27a36d0a07edb01a5b93b313f604fa9371d
sun4205 <hanna4205@gmail.com>
2023-04-09 07:58:08 -0400

    checking age practice

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const a= {
 name:"hanna123",
 address:"mt",
-address2:"mt2"
+address2:"mt2",
+address3:"mt3"
 
 }
 console.log(a.name);
 // i added 1 million code lines
+console.log(a.address);
+
+let age = 10
+function checkAge(){
+    if(age>20){
+        console.log("age is greater than 20");
+    }else if(age==20){
+        console.log("age is 20");
+    }else{
+        console.log("age is less than 20");
+    }
+}
+
+let checking = checkAge();
+console.log(checking);
