80c7351da3c2f90a1dfcde8261152b884de0b27c
sun4205 <hanna4205@gmail.com>
2023-04-10 21:25:06 -0400

    practice reduce

--- index.js
+++ index.js
@@ -54,3 +54,39 @@ var officers = [
 
   const officerIds = officers.map(officer =>officer.id);
   console.log(officerIds);
+
+  let nums = [424,322,53];
+  let subt = nums.reduce(myFunc);
+  console.log(subt);
+
+  function myFunc(total,nu){
+    return total-nu;
+  }
+
+  var pilots = [
+    {
+      id: 10,
+      name: "Poe Dameron",
+      years: 14,
+    },
+    {
+      id: 2,
+      name: "Temmin 'Snap' Wexley",
+      years: 30,
+    },
+    {
+      id: 41,
+      name: "Tallissan Lintra",
+      years: 16,
+    },
+    {
+      id: 99,
+      name: "Ello Asty",
+      years: 22,
+    }
+  ];
+
+  const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
+  console.log(totalYears);
+  
+  
