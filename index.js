de8916d4ddc2a750c3aa63e32916c4c1586f8817
sun4205 <hanna4205@gmail.com>
2023-04-11 20:31:32 -0400

    practice filter

--- index.js
+++ index.js
@@ -88,5 +88,34 @@ var officers = [
 
   const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
   console.log(totalYears);
+
+  var pilots = [
+    {
+      id: 2,
+      name: "Wedge Antilles",
+      faction: "Rebels",
+    },
+    {
+      id: 8,
+      name: "Ciena Ree",
+      faction: "Empire",
+    },
+    {
+      id: 40,
+      name: "Iden Versio",
+      faction: "Empire",
+    },
+    {
+      id: 66,
+      name: "Thane Kyrell",
+      faction: "Rebels",
+    }
+  ];
+
+  const rebels = pilots.filter(pilot=>pilot.faction==="Rebels");
+  const empire = pilots.filter(pilot=>pilot.faction==="Empire");
+
+  console.log(rebels);
+  console.log(empire);
   
   
