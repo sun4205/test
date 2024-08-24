a24e8d4b99bd3e4a5b9b0737ded371b3d2b06523
sun4205 <hanna4205@gmail.com>
2024-08-24 18:04:20 -0400

    practice searching string

--- app.js
+++ app.js
@@ -1,86 +1,75 @@
 //callback practice
 const jobTitles = [
-	"Wizard of Light Bulb Moments",
-    "Ghost Hunter",
-    "Champagne Tester",
-    "Legal Bank Robber"
+  "Wizard of Light Bulb Moments",
+  "Ghost Hunter",
+  "Champagne Tester",
+  "Legal Bank Robber",
 ];
-jobTitles.forEach(function(item){
-	console.log(item);
-})
+jobTitles.forEach(function (item) {
+  console.log(item);
+});
 
-const counter = [
-	"Emperor",
-    "King",
-    "Caesar",
-    "Pharaoh",
-    "Sultan",
-    "Tsar"
-]
+const counter = ["Emperor", "King", "Caesar", "Pharaoh", "Sultan", "Tsar"];
 
-const counterIndexed = counter.map(function(person,index){
-	return `${index + 1}.${person}`;
-})
+const counterIndexed = counter.map(function (person, index) {
+  return `${index + 1}.${person}`;
+});
 
 console.log(counterIndexed);
 
 const kings = [
-    "Louis I the Fair",
-    "Louis II the Stammerer",
-    "Louis III",
-    "Louis IV Transmarinus",
-    "Louis V the Do-Nothing"
+  "Louis I the Fair",
+  "Louis II the Stammerer",
+  "Louis III",
+  "Louis IV Transmarinus",
+  "Louis V the Do-Nothing",
 ];
 
-const kingsIndexed = kings.map(function(item,index,array){
-	const currentIndex = `(${(index+1)} of ${array.length})`;
-	return `${item} ${currentIndex}`;
-})
+const kingsIndexed = kings.map(function (item, index, array) {
+  const currentIndex = `(${index + 1} of ${array.length})`;
+  return `${item} ${currentIndex}`;
+});
 
-console.log(kingsIndexed)
+console.log(kingsIndexed);
 
 //sorting an array
 
 const myNumbers = [0, 3.14, 2.718, 13];
-myNumbers.sort(function(a,b){
-	return a - b;
-})
+myNumbers.sort(function (a, b) {
+  return a - b;
+});
 
 console.log(myNumbers);
 
-const diseases = [
-    "Mysophobia",
-    "Fear of missing out",
-    "Erythrophobia"
-];
+const diseases = ["Mysophobia", "Fear of missing out", "Erythrophobia"];
 
-diseases.sort(function(a,b){
-	a = a.toLowerCase();
-	b = b.toLowerCase();
+diseases.sort(function (a, b) {
+  a = a.toLowerCase();
+  b = b.toLowerCase();
 
-	if(a<b) return -1;
-	if(a>b) return 1;
+  if (a < b) return -1;
+  if (a > b) return 1;
 
-	return 0;
-})
+  return 0;
+});
 
 console.log(diseases);
 
 //functions are values
 
-const multiply = function(a,b){
-    return a * b;
-}
-multiply(2,3);
+const multiply = function (a, b) {
+  return a * b;
+};
+multiply(2, 3);
 
-const result = [1,2,3].reduce(multiply);
+const result = [1, 2, 3].reduce(multiply);
 console.log(result);
 
-function createFunction(){
-    function simpleFunction(){
-        console.log("hello world")
-    }
-    return simpleFunction
+function createFunction() {
+  function simpleFunction() {
+    console.log("hello world");
+  }
+  return simpleFunction;
 }
 const newFunction = createFunction();
 newFunction();
@@ -88,95 +77,116 @@ newFunction();
 //arrow function
 
 const shorterSingleAnecdote = (joke) => `I know only one jokes:${jokes}`;
-const colorHex = () => ({white:"#FFF"});
+const colorHex = () => ({ white: "#FFF" });
 
-const array = [1,2,3,4];
-const newArray = array.map(ele => ele * 2);
+const array = [1, 2, 3, 4];
+const newArray = array.map((ele) => ele * 2);
 
 //Default parameters
 
-function generateName(firstName, lastName, middleName = ""){
-    return `${firstName} ${middleName} ${lastName} `;
+function generateName(firstName, lastName, middleName = "") {
+  return `${firstName} ${middleName} ${lastName} `;
 }
 
-generateName('jane','Doe');
+generateName("jane", "Doe");
 
 //forEach() practice
 
 const how = ["harder", "better", "faster", "stronger"];
 
 how.forEach(function (item) {
-    console.log(item + ".");
-}); 
+  console.log(item + ".");
+});
 
 const how2 = ["harder", "better", "faster", "stronger"];
 
 for (let i = 0; i < how.length; i += 1) {
-    console.log(how[i] + '.');
+  console.log(how[i] + ".");
 }
 
 const people = [
-    "Steven Spielberg",
-    "Michael Bay",
-    "Robin Spielberg",
-    "Sasha Rebecca Spielberg",
-    "James Cameron"
-  ];
-  
-  const Spielberg = [];
-  
-  people.forEach(function (element) {
-    if(element.includes("Spielberg")){
-      Spielberg.push(element);
-    }
-  });
-  
-  console.log(Spielberg);
+  "Steven Spielberg",
+  "Michael Bay",
+  "Robin Spielberg",
+  "Sasha Rebecca Spielberg",
+  "James Cameron",
+];
 
-  const greekAlphabet = 'αβγδεζηθικλμνξοπρστυφχψω';
-console.log(greekAlphabet.length); 
+const Spielberg = [];
+
+people.forEach(function (element) {
+  if (element.includes("Spielberg")) {
+    Spielberg.push(element);
+  }
+});
+
+console.log(Spielberg);
+
+const greekAlphabet = "αβγδεζηθικλμνξοπρστυφχψω";
+console.log(greekAlphabet.length);
 
 const wrongWord = "expresso";
-console.log(wrongWord[1]); 
+console.log(wrongWord[1]);
 
 const password = "qwerty";
 
-if (password.length<8) {
+if (password.length < 8) {
   console.log("You call this a strong password?");
-  
 }
 
 const acrostic = [
-    "Give me your patience, sister, while I frame",
-    "Exact in capitals your golden name;",
-    "Or sue the fair Apollo and he will",
-    "Rouse from his heavy slumber and instill",
-    "Great love in me for thee and Poesy.",
-    "Imagine not that greatest mastery",
-    "And kingdom over all the Realms of verse,",
-    "Nears more to heaven in aught, than when we nurse",
-    "And surety give to love and Brotherhood.",
-    " ",
-    "Anthropophagi in Othello's mood;",
-    "Ulysses storm'd and his enchanted belt",
-    "Glow with the Muse, but they are never felt",
-    "Unbosom'd so and so eternal made,",
-    "Such tender incense in their laurel shade",
-    "To all the regent sisters of the Nine",
-    "As this poor offering to you, sister mine.",
-    " ",
-    "Kind sister! aye, this third name says you are;",
-    "Enchanted has it been the Lord knows where;",
-    "And may it taste to you like good old wine,",
-    "Take you to real happiness and give",
-    "Sons, daughters and a home like honied hive."
-  ];
-  
-  
-  let georgianaAugustaKeats = "";
-  
-  for (let i = 0; i < acrostic.length; i += 1) {
-   georgianaAugustaKeats += acrostic[i][0];
-  }
-  console.log(georgianaAugustaKeats);
-  
+  "Give me your patience, sister, while I frame",
+  "Exact in capitals your golden name;",
+  "Or sue the fair Apollo and he will",
+  "Rouse from his heavy slumber and instill",
+  "Great love in me for thee and Poesy.",
+  "Imagine not that greatest mastery",
+  "And kingdom over all the Realms of verse,",
+  "Nears more to heaven in aught, than when we nurse",
+  "And surety give to love and Brotherhood.",
+  " ",
+  "Anthropophagi in Othello's mood;",
+  "Ulysses storm'd and his enchanted belt",
+  "Glow with the Muse, but they are never felt",
+  "Unbosom'd so and so eternal made,",
+  "Such tender incense in their laurel shade",
+  "To all the regent sisters of the Nine",
+  "As this poor offering to you, sister mine.",
+  " ",
+  "Kind sister! aye, this third name says you are;",
+  "Enchanted has it been the Lord knows where;",
+  "And may it taste to you like good old wine,",
+  "Take you to real happiness and give",
+  "Sons, daughters and a home like honied hive.",
+];
+
+let georgianaAugustaKeats = "";
+
+for (let i = 0; i < acrostic.length; i += 1) {
+  georgianaAugustaKeats += acrostic[i][0];
+}
+console.log(georgianaAugustaKeats);
+
+console.log("Triple Peaks".indexOf("T"));
+console.log("espresso".indexOf("s"));
+console.log("espresso".indexOf("x"));
+const elements = "Earth, Air, Fire, Water";
+console.log(elements.indexOf("Fire"));
+console.log(
+  "Harry Potter and the Prisoner of Azkaban".includes("Harry Potter")
+);
+console.log("Teamwork".includes("I"));
+console.log("Vendetta".startsWith("V"));
+console.log("The perfect date".startsWith("Dinner and a movie"));
+
+const theRealEnd = "This is not the end";
+console.log(theRealEnd.endsWith("end"));
+let message = "I'm tired. I should of grabbed an expresso before this lesson.";
+
+if (message.includes("of")) {
+  console.log("sigh, mutter");
+}
+
+if (message.includes("ex")) {
+  console.log("eye roll");
+}
