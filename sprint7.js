5fe672ca2f3dd6effaa241611639ef679e1058e0
sun4205 <hanna4205@gmail.com>
2024-09-11 12:44:42 -0400

    OOP practice

--- sprint7.js
+++ sprint7.js
@@ -28,3 +28,49 @@ function like() {
   
   
   obj.method();
+
+  const powerToggle = function() {
+    this.power = !this.power;
+  }
+
+  function createSmartphone(owner, model, camera) {
+    const newSmartphone = {
+        owner,
+        model,
+        camera,
+        power: false,
+        powerToggle : powerToggle
+    }
+    return newSmartphone;
+  }
+
+  const smartPhone1 = createSmartphone("Jennifer","Snailboy2", false);
+  const smartPhone12 = createSmartphone("Rory", "Yandexz8", true);
+
+  smartPhone1.powerToggle();
+  console.log(smartPhone1.power);
+
+  function prefer() {
+    this.isLike = !this.isLike;
+  }
+
+  function getSongInfo() {
+    return `${this.artist} - ${this.title} (${this.releaseYear})`;
+  }
+
+  function createSongs (artist,title,releaseYear) {
+    const newSong = {
+    artist,
+    title,
+    releaseYear,
+    isLike : false,
+    prefer,
+    getSongInfo
+  }
+  return newSong;
+}
+
+const songex = createSongs("Mac Miller", "Circles", 2020);
+console.log(songex.getSongInfo());
+
+ 
