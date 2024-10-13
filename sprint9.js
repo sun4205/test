4ab4a070f98759ff5dea8d7075e75ac408c89dbc
sun4205 <hanna4205@gmail.com>
2024-10-12 21:13:41 -0400

    jason practice

--- sprint9.js
+++ sprint9.js
@@ -1,18 +1,58 @@
+fetch("https://example.com/users",{
+    method : "post",
+    headers : {
+        "Content-Type": "application/json"
+    },
+    body : JSON.stringify({
+        username : "authur"
+    })
+    
+});
+
 fetch("https://example.com")
   .then((res) => {
-    return res.json(); 
-  })
-  .then((data) => {
-    console.log(data.user.name); 
+    console.log(res);
   })
   .catch((err) => {
-    console.error("Error. Request failed");
+    console.error("Error. The request failed");
   });
 
-  fetch("https://example.com", {
-    method: "POST",
-    body: JSON.stringify({
-      name: "Paul",
-      age: 30
-    })
-  });
+  const songs = [
+    {
+      title: "ALOHAnet",
+      artist: "Palmbomen II"
+    },
+    {
+      title: "The Other Stranger",
+      artist: "Doxa Sinistra"
+    },
+    {
+      title: "Ariadna",
+      artist: "Kedr Livansky"
+    }
+  ];
+
+  const songJson = JASON.stringify(songs);
+  console.log(songJson);
+
+  const songsObject = JSON.parse(songsJSON);
+
+console.log(typeof songsObject); 
+console.log(songsObject[0].title);
+const user = {
+    name: "Paul",
+    age: 30,
+    hasDog: true,
+    dog: {
+      name: "Doge",
+      age: 6
+    }
+  };
+  
+  const userDeepCopy = JSON.parse(JSON.stringify(user));
+  
+  console.log(user === userDeepCopy); 
+  console.log(user.dog === userDeepCopy.dog); 
+  console.log(user.name === userDeepCopy.name); 
+  console.log(user.dog.name === userDeepCopy.dog.name); 
+  
