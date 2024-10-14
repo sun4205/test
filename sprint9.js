cf4be418bb437b888b0adbc49cad5d4b1327241c
sun4205 <hanna4205@gmail.com>
2024-10-14 19:42:30 -0400

    fetch practice

--- sprint9.js
+++ sprint9.js
@@ -64,5 +64,49 @@ const user = {
     })
   })
 
-  
+  function createPostMarkup(post){
+    return 
+    `<div class="post>
+    <p class="header__title>${post.title}</p>
+    <p class="header__text>${post.body}</>
+    
+    `;
+  }
+
+  function addPostToDom(container,markup){
+    container.addAdjacentHTML("afterbegin",markup);
+  }
+
+  function getPosts(){
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=>res.json())
+    .then((posts)=>{
+        posts.forEach((post)=>{
+            addPostToDom(
+                document.querySelector(".container"),
+                createPostMarkup(post)
+            );
+        });
+    });
+  }
+
+  getPosts();
+
+  const quoteElement = document.querySelector("div.quote");
+
+fetch("https://se-quotes-api.onrender.com/v1/quotes/random")
+    .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    
+      return Promise.reject(`Something went wrong: ${res.status}`);
+  })
+  .then((data) => {
+    quoteElement.textContent = data.quote;
+  })
+  .catch((err) => {
+    console.error(err); 
+  });
+
   
