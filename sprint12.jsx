df47c03487ed11ea73ae69e198b5f87f81dde120
sun4205 <hanna4205@gmail.com>
2024-11-22 13:36:35 -0500

    createserver practice

--- sprint12.jsx
+++ sprint12.jsx
@@ -1,16 +1,69 @@
 const http = require("http");
-const server = http.createServer((request, response) => {
-  console.log("The response is here");
-  console.log(request);
-  console.log(response);
+// const server = http.createServer((request, response) => {
+//   console.log("The response is here");
+//   console.log(request);
+//   console.log(response);
 
-  res.statusCode = 200;
-  res.statusMessage = "OK";
-  res.setHeader("Content-Type", "text/plain");
+//   res.statusCode = 200;
+//   res.statusMessage = "OK";
+//   res.setHeader("Content-Type", "text/plain");
 
-  res.write("Hello, ");
-  res.write("world!");
+//   res.write("Hello, ");
+//   res.write("world!");
 
-  res.end();
+//   res.end();
+// });
+// server.listen(3000);
+
+const http = require('http');
+const {PORT = 3000} = process.env;
+
+
+const markup = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Y do U 🖤 2 code</title>
+    <meta charset="UTF-8">
+    <style>
+      .container {
+        max-width: 500px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 auto;
+        padding-top: 100px;
+      }
+
+      p {
+        font-family: sans-serif;
+        font-size: 90px;
+        margin: 0;
+      }
+
+      img {
+        width: 512px;
+        margin-top: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <p><font color="red">Y</font>&nbsp;do<br>U&nbsp;🖤<br>2&nbsp;code</p>
+      <img src="https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_code.gif" alt="my code">
+    </div>
+  </body>
+  </html>
+`;
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+  res.end(markup);
+  
 });
-server.listen(3000);
+server.listen(PORT);
+
+
+
